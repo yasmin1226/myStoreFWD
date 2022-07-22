bd39145a16c646901ae8f4b271cdb53818c6cd03
yasmin <yasminfathy520@gmail.com>
2022-07-22 18:44:12 +0200

    readmi file

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/_services/cart.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  itemsLen = 0;
+  constructor(private cart: CartService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cart.getProducts().subscribe((res) => {
+      this.itemsLen = res.length;
+    });
+  }
 }
